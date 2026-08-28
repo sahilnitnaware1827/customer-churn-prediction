5e9e7245fadbeac0c2fe9737e1a1f4ca1ab33440
Sahil Nitnaware <sahilnitnaware71@gmail.com>
2026-08-28 20:17:37 +0530

    test: add API validation and prediction tests

--- api/main.py
+++ api/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
+from pydantic import BaseModel, Field
+from typing import Literal
 
 from src.predict import predict_churn
 
@@ -12,25 +13,53 @@ app = FastAPI(
 
 
 class CustomerInput(BaseModel):
-    gender: str
-    SeniorCitizen: int
-    Partner: str
-    Dependents: str
-    tenure: int
-    PhoneService: str
-    MultipleLines: str
-    InternetService: str
-    OnlineSecurity: str
-    OnlineBackup: str
-    DeviceProtection: str
-    TechSupport: str
-    StreamingTV: str
-    StreamingMovies: str
-    Contract: str
-    PaperlessBilling: str
-    PaymentMethod: str
-    MonthlyCharges: float
-    TotalCharges: float
+
+    gender: Literal["Male", "Female"]
+
+    SeniorCitizen: Literal[0, 1]
+
+    Partner: Literal["Yes", "No"]
+
+    Dependents: Literal["Yes", "No"]
+
+    tenure: int = Field(ge=0, le=72)
+
+    PhoneService: Literal["Yes", "No"]
+
+    MultipleLines: Literal["Yes", "No", "No phone service"]
+
+    InternetService: Literal["DSL", "Fiber optic", "No"]
+
+    OnlineSecurity: Literal["Yes", "No", "No internet service"]
+
+    OnlineBackup: Literal["Yes", "No", "No internet service"]
+
+    DeviceProtection: Literal["Yes", "No", "No internet service"]
+
+    TechSupport: Literal["Yes", "No", "No internet service"]
+
+    StreamingTV: Literal["Yes", "No", "No internet service"]
+
+    StreamingMovies: Literal["Yes", "No", "No internet service"]
+
+    Contract: Literal[
+        "Month-to-month",
+        "One year",
+        "Two year"
+    ]
+
+    PaperlessBilling: Literal["Yes", "No"]
+
+    PaymentMethod: Literal[
+        "Electronic check",
+        "Mailed check",
+        "Bank transfer (automatic)",
+        "Credit card (automatic)"
+    ]
+
+    MonthlyCharges: float = Field(gt=0)
+
+    TotalCharges: float = Field(ge=0)
 
 
 @app.get("/")
